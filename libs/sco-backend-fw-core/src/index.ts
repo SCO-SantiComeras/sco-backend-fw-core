233420d8da9d8951f8fa03da1736d04d9fe6d994
scomeras94 <scomeras@gmail.com>
2024-05-02 23:26:18 +0200

    ADD - Index exports

--- libs/sco-backend-fw-core/src/index.ts
+++ libs/sco-backend-fw-core/src/index.ts
@@ -4,3 +4,9 @@ export * from './sco-backend-fw-core-library.module';
 // Core
 export * from './sco-backend-fw-core/sco-backend-fw-core.module';
 export * from './sco-backend-fw-core/sco-backend-fw-core.service';
+
+// Config
+export * from './sco-backend-fw-core/config/sco-backend-fw-core.config';
+
+// Logger
+export * from './sco-backend-fw-core/logger/logger.service';
